87b2eccb014c2c71700bc37476002173b9558273
Mariana_C_Ruiz <mchierandaruiz0@gmail.com>
2021-11-02 02:19:20 -0300

    vcr recording modes

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -3,6 +3,11 @@ describe 'HTTParty' do
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
+  end
+  it 'content-type 2', vcr: { cassette_name: 'HTTParty/content-type', record: :new_episodes } do
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/3')
+    content_type = response.headers['content-type']
+    expect(content_type).to match(/application\/json/)
     #end
   end
 end
