c27465fdf9b6779ce721d5bcb756cd077f2bd73c
Mariana_C_Ruiz <mchierandaruiz0@gmail.com>
2021-11-02 01:49:21 -0300

    vcr 2

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,8 +1,5 @@
 describe 'HTTParty' do
-  it 'content-type', :vcr do
-    # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2")
-    #  .to_return(status: 200, body: "", headers: { 'content-type': 'application/json' })
-    #VCR.use_cassette('jsonplaceholder/posts') do
+  it 'content-type', vcr: { cassette_name: 'HTTParty/content-type' } do
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
