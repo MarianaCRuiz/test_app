9f98dbf7e1bb854bfa8ac8b34868c408279d35fb
Mariana_C_Ruiz <mchierandaruiz0@gmail.com>
2021-11-02 02:11:25 -0300

    vcr non-deterministic URLs

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,5 +1,5 @@
 describe 'HTTParty' do
-  it 'content-type', vcr: { cassette_name: 'HTTParty/content-type' } do
+  it 'content-type', vcr: { cassette_name: 'HTTParty/content-type', match_requests_on: [:body] } do
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
